0e29f61696dd28a84d45aead3eb3c6fc5a10c62b
Wajid <wajidsiddiqi12@gmail.com>
2023-05-16 23:15:23 +0500

    Adding args of constructor

--- deploy/01-deploy-lottery.js
+++ deploy/01-deploy-lottery.js
@@ -1,12 +1,40 @@
-const { network } = require("hardhat");
+const { network, ethers } = require("hardhat");
+const {
+  developmentChains,
+  networkConfig,
+} = require("../helper-hardhat-config");
+
+const VRF_SUB_FUND_AMMOUNT = ethers.utils.parseEther("2");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+  let vrfCoordinatorV2Address, subscriptionId;
 
+  if (developmentChains.includes(network.name)) {
+    const vrfCoordinatorV2Mock = await ethers.getContract(
+      "VRFCoordinatorV2Mock"
+    );
+    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
+    const transactionResponse = await vrfCoordinatorV2Mock.createSubscription();
+    const transactionReceipt = await transactionResponse.wait(1);
+    subscriptionId = transactionReceipt.events[0].args.subId;
+    await vrfCoordinatorV2Mock.fundSubscription(
+      subscriptionId,
+      VRF_SUB_FUND_AMMOUNT
+    );
+  } else {
+    vrfCoordinatorV2Address = networkConfig[chainId][vrfCoordinator];
+    subscriptionId = networkConfig[chainId][subscriptionId];
+  }
+
+  const entranceFee = networkConfig[chainId]["entranceFee"];
+  const gasLane = networkConfig[chainId]["gasLane"];
+  const args = [vrfCoordinatorV2Address, entranceFee, gasLane, subscriptionId];
   const lottery = await deploy("Lottery", {
     from: deployer,
-    args: [],
+    args: args,
     log: true,
     waitConfirmations: network.config.blockConfirmations || 1,
   });
