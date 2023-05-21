e54fc75e5d9dfcd509bc953e7d87cbe503e9a167
Wajid <wajidsiddiqi12@gmail.com>
2023-05-21 17:09:40 +0500

    unit testing

--- test/unit/Lottery.test.js
+++ test/unit/Lottery.test.js
@@ -7,26 +7,42 @@ const { assert, expect } = require("chai");
 
 !developmentChains.includes(network.name)
   ? describe.skip
-  : describe("Lottery unit test", async () => {
-      let lottery, vrfCoordinator;
+  : describe("Lottery unit test", () => {
+      let lottery, vrfCoordinator, lotteryEntranceFee, deployer;
       const chainId = network.config.chainId;
 
       beforeEach(async () => {
-        const { deployer } = await getNamedAccounts();
+        deployer = (await getNamedAccounts()).deployer;
         await deployments.fixture(["all"]);
         lottery = await ethers.getContract("Lottery", deployer);
         vrfCoordinator = await ethers.getContract(
           "VRFCoordinatorV2Mock",
           deployer
         );
+        lotteryEntranceFee = await lottery.getEntranceFee();
       });
 
-      describe("constructor", async () => {
+      describe("constructor", () => {
         it("Initializes the lottery correctly", async () => {
           const lotteryState = await lottery.getLotteryState();
           const interval = await lottery.getInterval();
           assert.equal(lotteryState.toString(), "0");
           assert.equal(interval.toString(), networkConfig[chainId]["interval"]);
+        });
+      });
+
+      describe("enterLottery", () => {
+        it("reverts when you don't pay enough", async () => {
+          await expect(lottery.enterLottery()).to.be.revertedWithCustomError(
+            lottery,
+            "Lottery__NotEnoughETHEntered"
+          );
+        });
+
+        it("adds players when they enter", async () => {
+          await lottery.enterLottery({ value: lotteryEntranceFee });
+          const playerFromContract = await lottery.getPlayers(0);
+          assert.equal(playerFromContract, deployer);
         });
       });
     });
